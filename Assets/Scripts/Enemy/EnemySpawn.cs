fb2328d371c752eb7109aa905ed661393d9da19d
MightyChipmunk <21qqq4@gmail.com>
2022-02-08 21:57:59 +0900

    Seoul97

---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public float interval = 3.0f;
+
+    public GameObject Enemy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", interval, interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        if (SpawnManager.isboss == false)
+        {
+            float Xpos = Random.Range(-6.5f, 6.5f);
+            Instantiate(Enemy, new Vector3(Xpos, 6.0f), Quaternion.identity);
+        }
+    }
+}
